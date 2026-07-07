e5a79596bf51fe1e750e57a4b3e365361e6d7c97
Caio Arrabal <caio.a.1998@gmail.com>
2026-07-06 23:24:13 -0300

    Sync - Lesson 94

---
+++ airbnb/models/dim/dim_long_term_listings.py
@@ -0,0 +1,6 @@
+# Creating a simple Python model
+def model(dbt, session):
+    listings = dbt.ref("dim_listings_cleansed")
+
+    return (listings.filter(listings["MINIMUM_NIGHTS"] >= 30)
+                   .select("LISTING_ID", "LISTING_NAME", "PRICE"))
